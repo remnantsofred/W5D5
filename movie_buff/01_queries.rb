8ab4b656b9f0b2ce24471f09a997a9c235620dc0
remnantsofred <remnantsofred@gmail.com>
2022-11-04 14:05:28 -0700

    Progress on part 1 Movie Buff

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -6,7 +6,9 @@ def it_was_ok
   # You can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
-
+  Movie
+    .select(:id, :title, :score)
+    .where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,7 +22,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
-
+  Movie
+    .select(:id, :title)
+    .joins(:actors, :castings)
+    .where('actors.name = \'Harrison Ford\' AND castings.ord != 1') 
+    .distinct
 end
 
 def biggest_cast
